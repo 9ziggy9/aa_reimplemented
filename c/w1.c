db055d051495885aec925a6e901f15927b6b98c3
Ziggy <davidarogers@protonmail.com>
2021-12-22 09:58:03 -0800

    adding c solutions

--- c/w1.c
+++ c/w1.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main(void) {
-    printf("Hello, World\n");
+// Favorite Food
+// Print out your favorite food
+char* fav_food(void) {
+    return "Crab Legs";
+}
+
+// Number Exercise
+// Write 5 examples of expressions using operations
+void some_operations(void) {
+    printf("%d\n", 4 + 2);
+    printf("%f\n", 4 + 2.2);
+    printf("%f\n", 4 / 2.33);
+    printf("%d\n", 5 % 2);
+    printf("%f\n", 2.55 - 1.441);
+}
+
+// Boolean Exercise
+// Write some boolean operations
+
+void bools_example(void) {
+    bool x = false;
+    x = true && false;
+    printf("%s\n", x ? "true" : "false");
+    x = true || false;
+    printf("%s\n", x ? "true" : "false");
+    x = !false;
+    printf("%s\n", x ? "true" : "false");
+}
+
+// Variables Exercise
+// Declare a variable called "first_name" and "last_name" yaddy yada
+void full_name(char* name) {
+    strcat(name, "David ");
+    strcat(name, "Rogers");
+}
+
+int main(int argc, char **argv) {
+    printf("%s\n", fav_food());
+    some_operations();
+    bools_example();
+
+    char final_name[100];
+    full_name(final_name);
+    printf("%s\n", final_name);
     return 0;
 }
